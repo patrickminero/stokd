57e37faa3df0f75066b69cf736efd6f3a8ae890f
Patrick Minero <patrickminerovalencia@gmail.com>
2021-02-16 17:23:43 +0100

    Improved seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,26 +15,29 @@ user1 = User.create(email: 'test@email.com', password: 'asdasd', password_confir
 user2 = User.create(email: 'test2@email.com', password: 'asdasd', password_confirmation: 'asdasd')
 user3 = User.create(email: 'test3@email.com', password: 'asdasd', password_confirmation: 'asdasd')
 
-categories = ['Shirt', 'Shoes', 'Pants']
-colors = ['White', "Red", "Yellow"]
+categories = ['Shirt', 'Shoes', 'Pants', 'Sweater', 'T-shirt', 'Underwear', 'Sweatpants', 'Socks', 'Tie', 'Suits', 'Belt', 'Wallet']
+colors = ['White', "Green", "Yellow", 'Blue', 'Brown', 'Grey', 'Black']
 size = ['sm', 'md', 'lg', 'xl']
 description = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum optio reprehenderit est ex dolorem ipsum! Voluptates omnis animi numquam ullam assumenda distinctio, vitae, magni dicta quis laboriosam dolorum. Dicta quas adipisci nam in commodi, explicabo aperiam. Rem dolore beatae deleniti commodi facere aliquid, odit voluptates adipisci dolorum sed? Tempora, soluta?'
-nike = Company.create!(name: 'nike inc', description: description, business_type: 'Retail', product_categories: categories, employees: [user1])
-apple = Company.create!(name: 'Apple', description: description, business_type: 'Retail', product_categories: categories, employees: [user2])
-cocacola = Company.create!(name: 'CocaCola', description: description, business_type: 'Retail', product_categories: categories, employees: [user3])
+nike = Company.create!(name: 'My Cloathing Store', description: description, business_type: 'Retail', product_categories: categories, employees: [user1])
+apple = Company.create!(name: 'Zara', description: description, business_type: 'Retail', product_categories: categories, employees: [user2])
+cocacola = Company.create!(name: 'Primark', description: description, business_type: 'Retail', product_categories: categories, employees: [user3])
 
 p "Created #{Company.count} companies"
-companies = Company.all
-companies.each do |company|
-    10.times do
+
+
+categories.each do |category|
+    
+    30.times do
+        company = Company.all.sample
         product = Product.create!(company_id: company.id, 
         description: description, 
         quantity: rand(30..100), 
         sku: rand(1000000..2000000).to_s, 
-        name: "#{categories.sample} #{colors.sample}", 
+        name: "#{colors.sample} #{categories.sample}", 
         features: {'size': size.sample},
         category: company.product_categories.sample, price: rand(5.00..100.00).ceil(2))
-        # product.save!
+        p "Created #{product.name}"
     end
 end
 
